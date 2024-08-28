6180dc93a3a7571b7373b617fc88ab053b655494
logesh-waran-p <logesh.7871ll@gmail.com>
2024-08-28 19:24:07 +0530

    update bondspot application with basic security have been done

---
+++ src/main/java/com/application/security/DemoSecurityConfig.java
@@ -0,0 +1,104 @@
+package com.application.security;
+
+import javax.sql.DataSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.password.CompromisedPasswordChecker;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.password.HaveIBeenPwnedRestApiPasswordChecker;
+
+@Configuration
+public class DemoSecurityConfig {
+	
+	
+	
+
+//	//add support for JDBC and Default role based authentication
+//	
+	/*@Bean
+	public UserDetailsManager userDetailsManager(DataSource dataSource) {
+		JdbcUserDetailsManager jdbcUserDetailsManager = new JdbcUserDetailsManager(dataSource);
+		
+		//define query to retrieve a user by username
+		jdbcUserDetailsManager.setUsersByUsernameQuery(
+				"select email, password, active from users where email=?"
+				);
+				
+		//define query to retrieve the roles by username
+		jdbcUserDetailsManager.setAuthoritiesByUsernameQuery(
+				"select email, role from roles where email=?"
+				);
+		return jdbcUserDetailsManager;
+	}*/
+	
+	
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+		http.authorizeHttpRequests(configurer ->
+				configurer
+				
+					.requestMatchers(HttpMethod.GET, "/api/user").permitAll()
+					.requestMatchers(HttpMethod.GET, "/api/user/**").authenticated()					
+					.requestMatchers(HttpMethod.POST, "/api/user").permitAll()
+					.requestMatchers(HttpMethod.PUT, "/api/user/**").hasRole("admin")
+					.requestMatchers(HttpMethod.DELETE, "/api/user/**").hasRole("admin")
+					
+					
+				);
+		
+		//use http basic authendication
+		http.httpBasic(Customizer.withDefaults());
+		
+		//disable cross site request forgery
+		http.csrf(csrf -> csrf.disable());
+		
+		return http.build();
+	}
+	
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+	}
+	
+	@Bean
+	public CompromisedPasswordChecker compromisedPasswordChecker() {
+		return new HaveIBeenPwnedRestApiPasswordChecker();
+	}
+	
+	
+	
+	
+	
+	
+	
+	/*@Bean
+	public InMemoryUserDetailsManager userDetailsManager() {
+		UserDetails john = User.builder()
+				.username("john")
+				.password("{noop}test123")
+				.roles("EMPLOYEE")
+				.build();
+		
+		UserDetails mary = User.builder()
+				.username("mary")
+				.password("{noop}test123")
+				.roles("EMPLOYEE", "MANAGER")
+				.build();
+		
+		UserDetails susan = User.builder()
+				.username("susan")
+				.password("{noop}test123")
+				.roles("EMPLOYEE", "MANAGER", "ADMIN")
+				.build();
+		
+		return new InMemoryUserDetailsManager(john, mary, susan);
+				
+	}*/
+}
